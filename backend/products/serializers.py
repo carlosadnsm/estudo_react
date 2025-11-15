292ae7c75f9d0398b29a63927d302a2d5fdfc476
unknown <carlosadnsm@gmail.com>
2025-11-15 18:39:33 -0300

    FEAT: Movendo as pastas do core e dos produtos para dentro da pasta de back

---
+++ backend/products/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price']
+
+    # Validações extras
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("O nome do produto não pode ser vazio.")
+        return value
+
+    def validate_price(self, value):
+        if value is None or value <= 0:
+            raise serializers.ValidationError("O preço deve ser maior que zero.")
+        return value
